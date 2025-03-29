e2dbdf9e07aee6ca6cdc7f828ac984ed22131028
Nethmamlshni <nethmamalshani2002@gmail.com>
2025-03-30 00:28:47 +0530

    passport add

---
+++ config/passportConfig.js
@@ -0,0 +1,47 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import User from '../Models/User.js';
+import dotenv from 'dotenv';
+dotenv.config();
+  
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  },
+  async (token, tokenSecret, profile, done) => {
+    try {
+      
+      let user = await User.findOne({ email: profile.emails[0].value });
+
+      if (!user) {
+        
+        user = new User({
+          firstname: profile.name.givenName,
+          lastname: profile.name.familyName,
+          email: profile.emails[0].value,
+          profile_image: profile.photos[0].value,
+          password: '',
+          role: 'user', 
+          confirmed: true,
+        });
+
+        await user.save();
+      }
+      done(null, user);
+    } catch (error) {
+      done(error, null);
+    }
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+  
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import passport from 'passport';
+import './config/passportConfig.js';
 import Userrouter from './Routes/userRoutes.js';
 import Productrouter from './Routes/productRoutes.js';
 import OrderRouter from './Routes/oderRoutes.js';
@@ -15,6 +17,33 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(passport.initialize());
+app.get('/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get('/auth/google/callback',
+    passport.authenticate('google', {
+      failureRedirect: '/login'
+    }),
+    (req, res) => {
+      res.redirect('/');
+    }
+  );
+
+  // Login route
+app.get('/login', (req, res) => {
+    res.send('Login page');
+  });
+  
+  // Logout route
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+  
 
 app.use('/api/user', Userrouter);
 app.use('/api/product', Productrouter);
